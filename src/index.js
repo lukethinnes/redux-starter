b810dedd7b51d7e9f763f51c5263fa817b15407d
Luke Thinnes <lukethinnes@yahoo.com>
2021-06-03 18:48:20 -0600

    Removed vitals.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
+
 
 const userReducer = (state={}, action) => {
   switch(action.type){
@@ -37,8 +37,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
